cd9ced9998c1c3b01a041ca0ce04cd75a958d0c0
Juan Luis Cortes Hernandez <luizhdz072@gmail.com>
2020-12-13 16:01:24 -0600

    js load data table

---
+++ src/javascript/loadTable.js
@@ -0,0 +1,39 @@
+(function () {
+
+	loadTable()
+	
+})();
+
+function loadTable(){
+	const table = document.querySelector("table#main_table")
+	var tbody =  table.querySelector("tbody")
+
+	fetch("/src/data/architecture.json")
+	.then(resp => resp.json())
+	.then(data => {
+		
+		for (const item of data) {
+			appendRow(tbody, [item.img ,item.name, item.country, item.description])			
+		}
+		
+		
+	})
+}
+
+function appendRow(tbody, cells){
+	var newRow = tbody.insertRow();
+	for (const cell of cells) {
+		var newCell = newRow.insertCell();
+		var content;
+		if(cell.split(".").pop() == "jpg" ){
+			content = document.createElement("img")
+			content.src = "/src/img/"+cell
+			content.className = "img-thumbnail"
+		}else{
+			content = document.createTextNode(cell);
+		}
+
+		newCell.appendChild(content);		
+		
+	}
+}
